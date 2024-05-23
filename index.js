4e2923e688e96569150724ee11f9c4a27bf79583
AMIRKIAYI <amirkiayi516@gmail.com>
2024-05-23 16:44:12 +0300

    index.js file update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Your code here
 const dorger = document.getElementById("dodger")
 document.addEventListener("keydown", function(e){
-    if(e.key ==="ArrowLeft"){
+   
+        const newbottom = dorger.style.bottom.replace("px", "");
+        const bottomInt = parseInt(newbottom)
+
+
         const newleft =dorger.style.left.replace("px", "");
         const leftint =parseInt(newleft);
-        console.log(leftint)
+        
+        if(e.key ==="ArrowLeft"){
             if(leftint >=0){
                 dorger.style.left = `${leftint-1}px`
 
@@ -15,11 +20,30 @@ document.addEventListener("keydown", function(e){
        
 
     }else if(e.key ==="ArrowRight"){
-        const newleft =dorger.style.left.replace("px", "");
-        const leftint =parseInt(newleft);
+        
         if(leftint <=360){
             dorger.style.left = `${leftint+1}px`
 
+        }else{
+            alert("game over")
+        }
+
+    }else if(e.key ==="ArrowDown"){
+        
+        if(bottomInt >= 0){
+            dorger.style.bottom = `${bottomInt-1}px`
+
+        }else{
+            alert("game over")
+        }
+    
+    }else if(e.key ==="ArrowUp"){
+        
+        if(bottomInt <=380){
+            dorger.style.bottom = `${bottomInt+1}px`
+
+        }else{
+            alert("game over")
         }
 
     }else{
